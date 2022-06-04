11f662e8cb7e1338b58499e33718f9194abbe413
dongHyo <kdhyo98@gmail.com>
2022-06-04 10:38:25 +0900

    feat: 주문조회V3.1 - 페이징과 한계 돌파

--- kdhyo/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ kdhyo/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -12,6 +12,7 @@ import jpabook.jpashop.repository.OrderSearch;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -38,15 +39,26 @@ public class OrderApiController {
 			.collect(Collectors.toList());
 	}
 
+	@GetMapping("/api/v3.1/orders")
+	public List<OrderDto> ordersV31(
+		@RequestParam(value = "offset", defaultValue = "0") int offset,
+		@RequestParam(value = "limit", defaultValue = "0") int limit) {
+		List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
+
+		return orders.stream()
+			.map(OrderDto::new)
+			.collect(Collectors.toList());
+	}
+
 	@Getter
 	static class OrderDto {
 
-		private Long orderId;
-		private String name;
-		private LocalDateTime orderDate;
-		private OrderStatus orderStatus;
-		private Address address;
-		private List<OrderItemDto> orderItems;
+		private final Long orderId;
+		private final String name;
+		private final LocalDateTime orderDate;
+		private final OrderStatus orderStatus;
+		private final Address address;
+		private final List<OrderItemDto> orderItems;
 
 		OrderDto(Order order) {
 			this.orderId = order.getId();
@@ -63,9 +75,9 @@ public class OrderApiController {
 	@Getter
 	static class OrderItemDto {
 
-		private String itemName;
-		private int orderPrice;
-		private int count;
+		private final String itemName;
+		private final int orderPrice;
+		private final int count;
 
 		OrderItemDto(OrderItem orderItem) {
 			this.itemName = orderItem.getItem().getName();
