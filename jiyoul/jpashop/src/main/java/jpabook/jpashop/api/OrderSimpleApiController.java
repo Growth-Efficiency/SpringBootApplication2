6b55485cc923aeec1a8bff449ce6e92713405928
JiYoul <wldbf97@naver.com>
2022-06-08 21:48:27 +0900

    [feat] 3일차 Entity 직접 노출 예시와 DTO로 바꿔보기.

--- jiyoul/jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ jiyoul/jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -3,8 +3,10 @@ package jpabook.jpashop.api;
 import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
-import jpabook.jpashop.repository.OrderRepository;
-import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.OrderRepository;
+import jpabook.jpashop.repository.order.OrderSearch;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryRepository;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,6 +26,7 @@ import java.util.stream.Collectors;
 public class OrderSimpleApiController {
 
     private final OrderRepository orderRepository;
+    private final OrderSimpleQueryRepository orderSimpleQueryRepository;
 
     @GetMapping("/api/v1/simple-orders")
     public List<Order> ordersV1() {
@@ -33,8 +36,19 @@ public class OrderSimpleApiController {
 
     @GetMapping("/api/v2/simple-orders")
     public List<SimpleOrderDto> ordersV2() {
-        return orderRepository.findAllByString(new OrderSearch())
-                .stream().map(SimpleOrderDto::new).collect(Collectors.toList());
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        return orders.stream().map(SimpleOrderDto::new).collect(Collectors.toList());
+    }
+
+    @GetMapping("/api/v3/simple-orders")
+    public List<SimpleOrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery();
+        return orders.stream().map(SimpleOrderDto::new).collect(Collectors.toList());
+    }
+
+    @GetMapping("/api/v4/simple-orders")
+    public List<OrderSimpleQueryDto> ordersV4() {
+        return orderSimpleQueryRepository.findOrderDto();
     }
 
     @Data
@@ -53,5 +67,4 @@ public class OrderSimpleApiController {
             address = order.getDelivery().getAddress();
         }
     }
-
 }
